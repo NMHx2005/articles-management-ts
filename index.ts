cbab2e0031294c03e2b099be0db1cfb2b60e1d6e
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-06 14:54:51 +0700

    Kết nối Database

--- index.ts
+++ index.ts
@@ -1,12 +1,27 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/database";
+
+dotenv.config();
+
+connectDatabase();
+
+import Article from "./models/acticel.model";
+
 
 const app: Express = express();
 const port: number = 3000;
 
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+
+    const articles = await Article.find({
+        deleted: false
+    });
+
+
     res.json({
-        articles: []
+        articles: articles
     });
 });
 
