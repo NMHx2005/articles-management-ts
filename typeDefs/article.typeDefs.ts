08b437db05e72283a94c56d74354ef52a5300be9
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-09 09:13:07 +0700

    Sắp xếp Article

--- typeDefs/article.typeDefs.ts
+++ typeDefs/article.typeDefs.ts
@@ -11,7 +11,10 @@ export const typeDefsArticle = gql`
         category: Category
     }
     type Query {
-        getListArticle: [Article],
+        getListArticle(
+            sortKey: String,
+            sortValue: String
+        ): [Article],
         getArticle(id: String): Article
     }
     type message {
