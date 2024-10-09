3caa335b443396d40604b90c25578e4fd3fa0ed3
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-09 17:28:47 +0700

    Tính năng Đăng ký

---
+++ helpers/generate.helper.ts
@@ -0,0 +1,9 @@
+export const generateRandomString = (length: number): string => {
+    const characters: string =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result: string = "";
+    for (let i: number = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
